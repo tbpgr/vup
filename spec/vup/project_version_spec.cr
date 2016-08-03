7e4e64239fdeb2bce38e5bba096df0112fbb9b11
hkdnet <hkdnet@users.noreply.github.com>
2016-08-03 21:16:41 +0900

    Add testcases

--- spec/vup/project_version_spec.cr
+++ spec/vup/project_version_spec.cr
@@ -2,14 +2,33 @@ require "../spec_helper"
 require "tempfile"
 
 describe Vup::ProjectVersion do
-  describe "#load_version_cr" do
-    # ::Dir.cd("fixture/single") do
-    #   vup = Vup::Up.new(Vup::SemanticVersions::PATCH)
-    #   vup.load_version_cr
-    #   vup.major.should eq(0)
-    #   vup.minor.should eq(1)
-    #   vup.patch.should eq(2)
-    #   vup.cr_version.should eq("0.1.2")
-    # end
+  describe "#version" do
+    it "returns major.minor.patch" do
+      pv = Vup::ProjectVersion.new
+      pv.major = 1
+      pv.minor = 2
+      pv.patch = 3
+      pv.version.should eq "1.2.3"
+    end
+  end
+
+  describe "#bumpup" do
+    kases = [
+      { case: Vup::SemanticVersions::PATCH, expected: "1.2.4" },
+      { case: Vup::SemanticVersions::MINOR, expected: "1.3.0" },
+      { case: Vup::SemanticVersions::MAJOR, expected: "2.0.0" }
+    ]
+    pv = Vup::ProjectVersion.new
+    pv.major = 1
+    pv.minor = 2
+    pv.patch = 3
+
+    kases.each do |k|
+      context k[:case] do
+        it "equals #{k[:expected]}" do
+          pv.dup.bumpup(k[:case]).version.should eq k[:expected]
+        end
+      end
+    end
   end
 end
